66c97768c6823f12d4bd4f303449dd0a69351263
Kielan2 <kielananderson@gmail.com>
2022-08-14 22:07:23 -0600

    auth & sidebar

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,10 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi()
 
 function App() {
-  const  [token, setToken] = useState(null)
+  const  [{token}, dispatch] = useDataLayerValue()
+  
 
   useEffect(() => {
     const hash = getTokenFromUrl()
@@ -12,19 +18,38 @@ function App() {
     const _token = hash.access_token
 
     if (_token) {
-      setToken(_token)
-    }
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      })
 
-  }, [])
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
+      })
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists: playlists,
+        })
+      })
+      
+    }
+  }, [ dispatch ])
+
 
   return (
     <div className="app">
-      {
-        token ? (
-      <h1>I am logged in</h1>
-        ) : (
+      {token ? 
+      <Player spotify={spotify} />
+         : 
       <Login />
-        )
       }
     </div>
   );
--- music-player/src/Login.js
+++ music-player/src/Login.js
@@ -10,8 +10,6 @@ function Login() {
             src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
             alt="Spotify Logo" />
         <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
-        
-
     </div>
   )
 }
